aa223dcef788a31e1a678f7c282c4b0d5b96f6bf
haxul <s.s.starodubov@yandex.ru>
2023-11-02 14:25:16 +0400

    init

---
+++ src/test/java/com/starodubov/seek/file/example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starodubov.seek.file.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
